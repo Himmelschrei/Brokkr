80ed399ffe54955af86bd14bdd6e80d87fc58f75
Carlos Pineda <himmelschre@gmail.com>
2023-08-07 12:14:17 +0200

    Add date input fields

--- src/app/project-application/project-application.component.ts
+++ src/app/project-application/project-application.component.ts
@@ -2,20 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectApplicationForm } from '../project-application-form';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-project-application',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule
   ],
   templateUrl: './project-application.component.html',
   styleUrls: ['./project-application.component.css']
 })
 export class ProjectApplicationComponent {
   currentDate: string = '';
+  minDate: Date;
+  minEndDate: Date
 
-  constructor() {  }
+  constructor() {
+    this.minDate = new Date(Date());
+    this.minEndDate = new Date;
+    console.log("This is the value of startDate: " + document.getElementById('startDate'));
+  }
 
   ngOnInit(): void {
     this.setCurrentDate();
@@ -33,6 +46,14 @@ export class ProjectApplicationComponent {
 
   private formatNumber(number: number): string {
     return number < 10 ? `0${number}` : `${number}`;
+  }
+
+  updateMinEndDate(startDate: Date) {
+    if (startDate) {
+      this.minEndDate = startDate;
+    } else {
+      this.minEndDate = new Date();
+    }
   }
   
   submitted = false;
